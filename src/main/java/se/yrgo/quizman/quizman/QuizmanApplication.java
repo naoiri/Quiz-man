47976f9b9b55dc191b002909817b1cf3e2345af0
suzyrgo <suztra0803@skola.goteborg.se>
2021-11-22 13:35:28 +0100

    first commit

---
+++ src/main/java/se/yrgo/quizman/quizman/QuizmanApplication.java
@@ -0,0 +1,13 @@
+package se.yrgo.quizman.quizman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/se/yrgo/quizman/quizman/QuizmanApplicationTests.java
@@ -0,0 +1,13 @@
+package se.yrgo.quizman.quizman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
