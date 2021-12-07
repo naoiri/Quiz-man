b2d1a18af27a6adce3785980be95e4a49b77bd38
Isha0914 <shahid.ismail0914@gmail.com>
2021-12-07 16:37:41 +0100

    replaced code in Game.js with code in Test.js and routed in Game.js

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -1,115 +1,51 @@
-import { FaLaughBeam } from 'react-icons/fa';
-import { FaSmile } from 'react-icons/fa';
-import { FaGrinBeamSweat } from 'react-icons/fa';
-
-import { IconContext } from "react-icons";
-
+import './Test.css';
+import Axios from 'axios';
 import { useState } from "react";
+import React, { useEffect } from "react";
+
 
 const Game = () => {
-
-    const [text, setText] = useState('Answer here')
+    const url = 'http://localhost:8080/questionAnswers';
+    const [question, setQuestion] = useState("");
+    const [correctAnswer, setCorrectAnswer] = useState("");
+    const [answer2, setAnswer2] = useState("");
+    const [answer3, setAnswer3] = useState("");
+    const [answer4, setAnswer4] = useState("");
+    const [questionArray, setQuestionArray] = useState("");
+    const [text, setText] = useState("");
+    useEffect(() => {
+        Axios.get(url).then(response => {
+            setQuestionArray(response.data.questionAnswers)
+            setQuestion(response.data.questionAnswers[0].question);
+            setCorrectAnswer(response.data.questionAnswers[0].correctAnswer)
+            setAnswer2(response.data.questionAnswers[0].answer2)
+            setAnswer3(response.data.questionAnswers[0].answer3)
+            setAnswer4(response.data.questionAnswers[0].answer4)
+        })
+    }, [url])
     const checkAnswer = (e) => {
+        console.log(correctAnswer)
+        console.log(questionArray)
+        console.log(e)
 
-        if(e.target.innerText === 'Apa'){
-            setText('Rätt!')
-        } else {
-            setText('Fel!')
+        console.log(e.target.innerText)
+        if(e.target.innerText === correctAnswer){
+            setText("Yeaboii")
         }
+        else{
+            setText("Noboii")
+        }
+
     }
-
-    const questions = [
-        {
-            questionText: 'What is the capital of France?',
-            answerOptions: [
-                { answerText: 'New York', isCorrect: false },
-                { answerText: 'London', isCorrect: false },
-                { answerText: 'Paris', isCorrect: true },
-                { answerText: 'Dublin', isCorrect: false },
-            ],
-        },
-        {
-            questionText: 'Who is CEO of Tesla?',
-            answerOptions: [
-                { answerText: 'Jeff Bezos', isCorrect: false },
-                { answerText: 'Elon Musk', isCorrect: true },
-                { answerText: 'Bill Gates', isCorrect: false },
-                { answerText: 'Tony Stark', isCorrect: false },
-            ],
-        },
-        {
-            questionText: 'The iPhone was created by which company?',
-            answerOptions: [
-                { answerText: 'Apple', isCorrect: true },
-                { answerText: 'Intel', isCorrect: false },
-                { answerText: 'Amazon', isCorrect: false },
-                { answerText: 'Microsoft', isCorrect: false },
-            ],
-        },
-        {
-            questionText: 'How many Harry Potter books are there?',
-            answerOptions: [
-                { answerText: '1', isCorrect: false },
-                { answerText: '4', isCorrect: false },
-                { answerText: '6', isCorrect: false },
-                { answerText: '7', isCorrect: true },
-            ],
-        },
-    ];
-
     return (
-        <div className= "Game" style={{ fontSize: "30px" }}>
-            
-            <div className="figure" style={{
-                fontSize: "30px",
-                marginBottom: "10px"
-            }}>Figure Area!
-                <IconContext.Provider value={{ color: "#ff6b6b", size: "50px", backgroundColor: "red" }}>
-                    <div>
-                        <FaLaughBeam />
-                        <FaSmile />
-                        <FaGrinBeamSweat />
-                    </div>
-                </IconContext.Provider>
-               
-               
-            </div>
-            <div className="question"> 
-                <p >Vad heter apan?</p>
-            </div>
-            <div className="text">
-               <button onClick={(e)=> checkAnswer(e)}>Apa</button>
-               <button onClick={(e)=> checkAnswer(e)}>Bpa</button>
-               <button onClick={(e)=> checkAnswer(e)}>Cpa</button>
-               <button onClick={(e)=> checkAnswer(e)}>Dpa</button>
-               <p>{text}</p>
-
-            </div>
-            <div className='app'>
-            {/* HINT: replace "false" with  oncklick logic to display the 
-      score when the user has answered all the questions */}
-            {false ? (
-                <div className='score-section'>You scored 1 out of {questions.length}</div>
-            ) : (
-                <>
-                    <div className='question-section'>
-                        <div className='question-count'>
-                            <span>Question 1</span>/{questions.length}
-                        </div>
-                        <div className='question-text'>This is where the question text should go</div>
-                    </div>
-                    <div className='answer-section'>
-                        <button>Answer 1</button>
-                        <button>Answer 2</button>
-                        <button>Answer 3</button>
-                        <button>Answer 4</button>
-                    </div>
-                </>
-            )}
+        <div className="grid-container">
+            <div className="grid-item grid-item-1">{question}</div>
+            <div className="grid-item grid-item-2" onClick={checkAnswer}>{correctAnswer} </div>
+            <div className="grid-item grid-item-3"  onClick={checkAnswer}>{answer2}</div>
+            <div className="grid-item grid-item-4"  onClick={checkAnswer}>{answer3}</div>
+            <div className="grid-item grid-item-5"  onClick={checkAnswer}>{answer4}</div>
+            <div className="grid-item grid-item-6" onClick={checkAnswer}>{text}</div>
         </div>
-        </div>
-
-    );
+    )
 }
-
 export default Game;
