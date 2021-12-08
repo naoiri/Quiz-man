24dba24b7c71ffbd1c8a4194989da324a88d06a4
Isha0914 <shahid.ismail0914@gmail.com>
2021-12-08 21:12:49 +0100

    change the SQL from H2 to MSSQL

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -11,22 +11,31 @@ const Game = () => {
     const [answer2, setAnswer2] = useState("");
     const [answer3, setAnswer3] = useState("");
     const [answer4, setAnswer4] = useState("");
-    const [questionArray, setQuestionArray] = useState("");
+    const [questionArray, setQuestionArray] = useState([]);
+    const [currentQuestion, setCurrentQuestion] = useState(0)
     const [text, setText] = useState("");
+
     useEffect(() => {
         Axios.get(url).then(response => {
             setQuestionArray(response.data.questionAnswers)
-            setQuestion(response.data.questionAnswers[0].question);
-            setCorrectAnswer(response.data.questionAnswers[0].correctAnswer)
-            setAnswer2(response.data.questionAnswers[0].answer2)
-            setAnswer3(response.data.questionAnswers[0].answer3)
-            setAnswer4(response.data.questionAnswers[0].answer4)
+            setQuestion(response.data.questionAnswers[currentQuestion].question);
+            setCorrectAnswer(response.data.questionAnswers[currentQuestion].correctAnswer)
+            setAnswer2(response.data.questionAnswers[currentQuestion].answer2)
+            setAnswer3(response.data.questionAnswers[currentQuestion].answer3)
+            setAnswer4(response.data.questionAnswers[currentQuestion].answer4)
         })
     }, [url])
+
+
+    const handleAnswerButtonClick = () => {
+    const nextQuestion = currentQuestion + 1;
+    setCurrentQuestion(nextQuestion)
+    }
     const checkAnswer = (e) => {
         console.log(correctAnswer)
         console.log(questionArray)
         console.log(e)
+        console.log(question)
 
         console.log(e.target.innerText)
         if(e.target.innerText === correctAnswer){
@@ -39,12 +48,13 @@ const Game = () => {
     }
     return (
         <div className="grid-container">
-            <div className="grid-item grid-item-1">{question}</div>
-            <div className="grid-item grid-item-2" onClick={checkAnswer}>{correctAnswer} </div>
-            <div className="grid-item grid-item-3"  onClick={checkAnswer}>{answer2}</div>
-            <div className="grid-item grid-item-4"  onClick={checkAnswer}>{answer3}</div>
-            <div className="grid-item grid-item-5"  onClick={checkAnswer}>{answer4}</div>
-            <div className="grid-item grid-item-6" onClick={checkAnswer}>{text}</div>
+            <div className="grid-item grid-item-1" onClick={handleAnswerButtonClick}>{question}</div>
+            <div className="grid-item grid-item-2" onClick={handleAnswerButtonClick}>{correctAnswer} </div>
+            <div className="grid-item grid-item-3"  onClick={handleAnswerButtonClick}>{answer2}</div>
+            <div className="grid-item grid-item-4"  onClick={handleAnswerButtonClick}>{answer3}</div>
+            <div className="grid-item grid-item-5"  onClick={handleAnswerButtonClick}>{answer4}</div>
+            <div className="grid-item grid-item-6" onClick={handleAnswerButtonClick}>{text}</div>
+            {questionArray.map(questionArray => <div className="grid-item grid-item-7">{questionArray.question}</div>)}
         </div>
     )
 }
