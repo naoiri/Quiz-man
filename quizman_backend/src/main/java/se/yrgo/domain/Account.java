c1f88e153aa01d8c82fae0edc12fadce94f28477
suzyrgo <suztra0803@skola.goteborg.se>
2021-12-10 15:31:03 +0100

    modified application.properties and created column in Account.java

--- quizman_backend/src/main/java/se/yrgo/domain/Account.java
+++ quizman_backend/src/main/java/se/yrgo/domain/Account.java
@@ -3,6 +3,7 @@ package se.yrgo.domain;
 import javax.persistence.*;
 
 @Entity
+@Table(name= "player")
 public class Account {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +22,40 @@ public class Account {
         this.highscore = highscore;
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public  long getId(){
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Column(name = "email", nullable = false)
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Column(name = "password", nullable = false)
+    public String getPassword(){
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Column(name = "highscore", nullable = false)
+    public int getHighscore() {
+        return highscore;
+    }
+    public void setHighscore(int highscore) {
+        this.highscore = highscore;
+    }
+
+
     @Override
     public String toString() {
         return "Account{" +
@@ -31,35 +66,5 @@ public class Account {
                 '}';
     }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getHighscore() {
-        return highscore;
-    }
-
-    public void setHighscore(int highscore) {
-        this.highscore = highscore;
-    }
 }
