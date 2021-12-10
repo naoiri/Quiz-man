c50a2534c1bfb8c890d634750fff2ce85cc325bf
suzyrgo <suztra0803@skola.goteborg.se>
2021-12-10 18:16:23 +0100

    added get,delete and putmapping

--- quizman_backend/src/main/java/se/yrgo/data/AccountRepository.java
+++ quizman_backend/src/main/java/se/yrgo/data/AccountRepository.java
@@ -4,6 +4,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.yrgo.domain.Account;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    static Optional<Object> findId(Long id) {
+        return null;
+    }
+
+    static List<Account> finndAll() {
+        return null;
+    }
 }
--- quizman_backend/src/main/java/se/yrgo/rest/AccountRestController.java
+++ quizman_backend/src/main/java/se/yrgo/rest/AccountRestController.java
@@ -1,22 +1,23 @@
 package se.yrgo.rest;
 
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import se.yrgo.data.AccountRepository;
 import se.yrgo.domain.Account;
 
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class AccountRestController {
 
     @Autowired
-    public AccountRepository data;
+    private AccountRepository data;
 
     @RequestMapping("/accounts")
     public AccountList allAccounts(){
@@ -29,5 +30,47 @@ public class AccountRestController {
         data.save(account);
         return new ResponseEntity<>(account, HttpStatus.CREATED);
     }
+    @PutMapping("/accounts/{id}")
+    public ResponseEntity<Account>getId(@PathVariable(value = "id") Long id,
+         @RequestBody Account accountInfo) throws ResourceNotFoundException {
+        Account account = (Account) AccountRepository.findId(id)
+                .orElseThrow(() -> new
+                        ResourceNotFoundException( "Player nor found by this id ::" + id));
+        account.setEmail(accountInfo.getEmail());
+        account.setPassword((accountInfo.getPassword()));
+        account.setHighscore(accountInfo.getHighscore());
+        final Account updateAccount = data.save(account);
+        return ResponseEntity.ok(updateAccount);
+
+
+    }
+    @GetMapping("/accounts")
+    public List<Account>getAccounts(){
+        return AccountRepository.finndAll();
+
+    }
+    @GetMapping("/accounts/{id}")
+    public ResponseEntity<Account>getId(@PathVariable(value = "id") Long id)
+            throws ResourceNotFoundException {
+        Account account = (Account) AccountRepository.findId(id)
+                .orElseThrow(() -> new
+                        ResourceNotFoundException( "Player not found by this id ::" + id));
+        return ResponseEntity.ok().body(account);
+
+    }
+    @DeleteMapping("(accounts/{id}")
+    public Map<String, Boolean> deleteAccount(@PathVariable(value = "id") Long accountId)
+        throws ResourceNotFoundException {
+        Account account= (Account) AccountRepository.findId(accountId)
+                .orElseThrow(() -> new ResourceNotFoundException("Player not found by this id :: " + accountId));
+                data.delete(account);
+                Map<String, Boolean> response = new HashMap<>();
+                response.put("deleted" , Boolean.TRUE);
+                return response;
+    }
+
+
+
+
 
 }
