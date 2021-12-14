1056eecafe724c8ce3c1ad5f0810f018a4c5b1d7
Naoya Irikura <naoiri2001@skola.goteborg.se>
2021-12-14 18:14:53 +0100

    Create a method which gets all the movie questions

--- quizman_backend/src/main/java/se/yrgo/data/QuestionRepository.java
+++ quizman_backend/src/main/java/se/yrgo/data/QuestionRepository.java
@@ -3,7 +3,12 @@ package se.yrgo.data;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import se.yrgo.domain.QuestionAnswer;
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<QuestionAnswer, Long> {
+
+    @Query("SELECT qa FROM QuestionAnswer qa WHERE qa.category = 1")
+    List<QuestionAnswer> getMovieQuestions();
 }
