6294f77f54222514628090a7138960fd934a51e0
suzyrgo <suztra0803@skola.goteborg.se>
2021-12-17 16:13:58 +0100

    add functionality to get a single account

--- quizman_backend/src/main/java/se/yrgo/domain/Account.java
+++ quizman_backend/src/main/java/se/yrgo/domain/Account.java
@@ -1,70 +1,36 @@
 package se.yrgo.domain;
-
 import javax.persistence.*;
-
 @Entity
-@Table(name= "player")
 public class Account {
-
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
     private String email;
     private String password;
     private int highscore;
-
     public Account(){}
-
-    public Account(Long id, String email, String password, int highscore) {
-        this.id = id;
-        this.email = email;
-        this.password = password;
-        this.highscore = highscore;
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public  long getId(){
+    public Long getId() {
         return id;
     }
     public void setId(Long id) {
         this.id = id;
     }
-
-    @Column(name = "email", nullable = false)
-    public String getEmail(){
+    public String getEmail() {
         return email;
     }
     public void setEmail(String email) {
         this.email = email;
     }
-
-    @Column(name = "password", nullable = false)
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
     public void setPassword(String password) {
         this.password = password;
     }
-
-    @Column(name = "highscore", nullable = false)
     public int getHighscore() {
         return highscore;
     }
     public void setHighscore(int highscore) {
         this.highscore = highscore;
     }
-
-
-    @Override
-    public String toString() {
-        return "Account{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", password='" + password + '\'' +
-                ", highscore=" + highscore +
-                '}';
-    }
-
-
 }
--- quizman_backend/src/main/java/se/yrgo/rest/AccountRestController.java
+++ quizman_backend/src/main/java/se/yrgo/rest/AccountRestController.java
@@ -1,6 +1,5 @@
 package se.yrgo.rest;
 
-import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +8,8 @@ import se.yrgo.data.AccountRepository;
 import se.yrgo.domain.Account;
 
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.Optional;
 
 @RestController
 public class AccountRestController {
@@ -20,7 +18,7 @@ public class AccountRestController {
     private AccountRepository data;
 
     @RequestMapping("/accounts")
-    public AccountList allAccounts(){
+    public AccountList allAccounts() {
         List<Account> accounts = data.findAll();
         return new AccountList(accounts);
     }
@@ -30,47 +28,10 @@ public class AccountRestController {
         data.save(account);
         return new ResponseEntity<>(account, HttpStatus.CREATED);
     }
-    @PutMapping("/accounts/{id}")
-    public ResponseEntity<Account>getId(@PathVariable(value = "id") Long id,
-         @RequestBody Account accountInfo) throws ResourceNotFoundException {
-        Account account = (Account) AccountRepository.findId(id)
-                .orElseThrow(() -> new
-                        ResourceNotFoundException( "Player nor found by this id ::" + id));
-        account.setEmail(accountInfo.getEmail());
-        account.setPassword((accountInfo.getPassword()));
-        account.setHighscore(accountInfo.getHighscore());
-        final Account updateAccount = data.save(account);
-        return ResponseEntity.ok(updateAccount);
 
-
+    @RequestMapping("/accounts/{id}")
+    public Optional<Account> getAccountByID(@PathVariable Long id) {
+        Optional<Account> account = data.findById(id);
+        return account;
     }
-    @GetMapping("/accounts")
-    public List<Account>getAccounts(){
-        return AccountRepository.finndAll();
-
-    }
-    @GetMapping("/accounts/{id}")
-    public ResponseEntity<Account>getId(@PathVariable(value = "id") Long id)
-            throws ResourceNotFoundException {
-        Account account = (Account) AccountRepository.findId(id)
-                .orElseThrow(() -> new
-                        ResourceNotFoundException( "Player not found by this id ::" + id));
-        return ResponseEntity.ok().body(account);
-
-    }
-    @DeleteMapping("(accounts/{id}")
-    public Map<String, Boolean> deleteAccount(@PathVariable(value = "id") Long accountId)
-        throws ResourceNotFoundException {
-        Account account= (Account) AccountRepository.findId(accountId)
-                .orElseThrow(() -> new ResourceNotFoundException("Player not found by this id :: " + accountId));
-                data.delete(account);
-                Map<String, Boolean> response = new HashMap<>();
-                response.put("deleted" , Boolean.TRUE);
-                return response;
-    }
-
-
-
-
-
 }
