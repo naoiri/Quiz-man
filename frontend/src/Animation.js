669f84d117ee8cb2312cccfdd76de48e61b9ea2c
Naoya Irikura <naoiri2001@skola.goteborg.se>
2022-01-07 00:42:39 +0100

    Add css element for fa-icons in Test.css

--- frontend/src/Animation.js
+++ frontend/src/Animation.js
@@ -2,15 +2,20 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSmile, faMeh, faFrown, faFlushed, faDizzy  } from '@fortawesome/free-solid-svg-icons';
 
+
 function Animation() {
     return (
         <div>
            <h1>This is the animation component!</h1>
-            <FontAwesomeIcon icon={faSmile}/>
-            <FontAwesomeIcon icon={faMeh}/>
-            <FontAwesomeIcon icon={faFrown}/>
-            <FontAwesomeIcon icon={faFlushed}/>
-            <FontAwesomeIcon icon={faDizzy}/>
+            <div >
+                <FontAwesomeIcon icon={faSmile} className="fa-icons"/>
+                <FontAwesomeIcon icon={faMeh} className="fa-icons"/>
+                <FontAwesomeIcon icon={faFrown} className="fa-icons"/>
+                <FontAwesomeIcon icon={faFlushed} className="fa-icons"/>
+                <FontAwesomeIcon icon={faDizzy} className="fa-icons"/>
+
+            </div>
+
         </div>
     );
 }
