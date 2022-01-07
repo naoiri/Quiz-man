2a40eeb9b8c1491db9b67dfdd509d429806d70d7
Naoya Irikura <naoiri2001@skola.goteborg.se>
2022-01-07 01:08:57 +0100

    Add hardcoded face change

--- frontend/src/Animation.js
+++ frontend/src/Animation.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSmile, faMeh, faFrown, faFlushed, faDizzy  } from '@fortawesome/free-solid-svg-icons';
 
 
 function Animation() {
+
+    const [time, setTime] = useState(20);
     return (
         <div>
+            {(() => {
+                if (time<10) {
+                    return (
+                        <FontAwesomeIcon icon={faDizzy} className="fa-icons"/>
+                    )
+                } else if (time<20) {
+                    return (
+                        <FontAwesomeIcon icon={faFlushed} className="fa-icons"/>
+                    )
+                } else if (time<30){
+                    return (
+                        <FontAwesomeIcon icon={faFrown} className="fa-icons"/>
+                    )
+                } else if (time<40){
+                    return (
+                        <FontAwesomeIcon icon={faMeh} className="fa-icons"/>
+                    )
+                } else if (time<50){
+                    return (
+                        <FontAwesomeIcon icon={faSmile} className="fa-icons"/>
+                    )
+                }
+            })()}
+
            <h1>This is the animation component!</h1>
             <div >
                 <FontAwesomeIcon icon={faSmile} className="fa-icons"/>
