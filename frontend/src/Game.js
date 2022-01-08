503d501fadf06d95ab5e3b0d253179cb061c76bc
Naoya Irikura <naoiri2001@skola.goteborg.se>
2022-01-08 21:59:09 +0100

    Show the answers randomized as in database

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -20,12 +20,13 @@ const Game = () => {
     useEffect(() => {
         Axios.get(url).then(response => {
             setQuestionAnswers(response.data.questionAnswers)
+            console.log(response.data.questionAnswers)
 
         })
     }, [url])
 
     const handleAnswer = (e) => {
-        if (e.target.innerText === questionAnswers[currentIndex].correctAnswer.trim()) {
+        if (e.target.innerText === questionAnswers[currentIndex].correctAnswer?.trim()) {
             setText("Correct!")
             setScore(score + 1)
 
@@ -50,9 +51,6 @@ const Game = () => {
     }
 
     const gameAreaSwitch = () =>{ //This function returns a component
-        const singleQuestion = getSingleQuestion(currentIndex)
-        const randomizedAnswers = randomize(singleQuestion);
-
         if(timer===0){
             return (<div className="score-board">Time is up! Hangman is now dead. Your score: {score}</div>)
         } else if (scoreBoard) {
@@ -63,44 +61,16 @@ const Game = () => {
                 <div className='quiz-container'>
                     <div className='question-text'>{questionAnswers[currentIndex]?.question}</div>
                     <div className='answer-section'>
-                        <button onClick={handleAnswer}>{randomizedAnswers[0]}</button>
-                        <button onClick={handleAnswer}>{randomizedAnswers[1]}</button>
-                        <button onClick={handleAnswer}>{randomizedAnswers[2]}</button>
-                        <button onClick={handleAnswer}>{randomizedAnswers[3]}</button>
+                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.answer1}</button>
+                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.answer2}</button>
+                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.answer3}</button>
+                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.answer4}</button>
                         <div>{text}</div>
                     </div>
                 </div>
             )
         }
 
-    }
-
-    const getSingleQuestion = (currentIndex) => {
-        const singleQuestion = questionAnswers[currentIndex];
-       return singleQuestion;
-    }
-
-
-    const randomize = (singleQuestion) =>{
-        const answers = [
-            singleQuestion?.correctAnswer,
-            singleQuestion?.answer2,
-            singleQuestion?.answer3,
-            singleQuestion?.answer4
-        ]
-
-        for(let i = (answers.length - 1); 0 < i; i--){
-
-            // 0〜(i+1)の範囲で値を取得
-            let random = Math.floor(Math.random() * (i + 1));
-
-            // 要素の並び替えを実行
-            let tmp = answers[i];
-            answers[i] = answers[random];
-            answers[random] = tmp;
-        }
-
-        return answers;
     }
 
     return (
