18f46d3371cca460d8416c1cf8af3a746ec6615b
Naoya Irikura <naoiri2001@skola.goteborg.se>
2022-01-08 03:30:04 +0100

    Refactor. Create  Game area switch function to make jsx rendering more simple

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -1,12 +1,12 @@
 import './Test.css';
 import Axios from 'axios';
-import { useState } from "react";
-import React, { useEffect } from "react";
+import {useState} from "react";
+import React, {useEffect} from "react";
 import Animation from "./Animation";
 
 const Game = () => {
 
-    const[timer, setTimer] = useState();
+    const [timer, setTimer] = useState();
 
     const [url, setUrl] = useState('')
     const [chosenCategory, setChosenCategory] = useState(null)
@@ -24,18 +24,17 @@ const Game = () => {
     }, [url])
 
     const handleAnswer = (e) => {
-        if(e.target.innerText === questionAnswers[currentIndex].correctAnswer.trim()){
+        if (e.target.innerText === questionAnswers[currentIndex].correctAnswer.trim()) {
             setText("Correct!")
             setScore(score + 1)
 
-        }
-        else{
+        } else {
             setText("Incorrect...")
         }
 
         const nextIndex = currentIndex + 1;
 
-        if(nextIndex < questionAnswers.length){
+        if (nextIndex < questionAnswers.length) {
             setCurrentIndex(nextIndex);
         } else {
             showScoreBoard(true)
@@ -43,53 +42,55 @@ const Game = () => {
 
     }
 
-    const handleCategory = (e) =>{
+    const handleCategory = (e) => {
         const category = e.target.innerText.toLowerCase();
-        setUrl('http://localhost:8080/questionAnswers/'+ category);
+        setUrl('http://localhost:8080/questionAnswers/' + category);
         setChosenCategory(category);
     }
+
+    const GameAreaSwitch = ()=>{ //This function returns a component.
+        if(timer===0){
+            return (<div className="score-board">Time is up! Hangman is now dead. Your score: {score}</div>)
+        } else if (scoreBoard){
+            return (<div className="score-board">Hangman survived! Your score: {score}</div>)
+        } else {
+            return (
+                <div className='quiz-container'>
+                    <div className='question-text'>{questionAnswers[currentIndex]?.question}</div>
+                    <div className='answer-section'>
+                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.correctAnswer}</button>
+                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.answer2}</button>
+                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.answer3}</button>
+                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.answer4}</button>
+                        <div>{text}</div>
+                    </div>
+                </div>
+            )
+        }
+
+    }
+
 
     return (
         <div>
 
-            {chosenCategory==null ? (
-                <div >
+            {chosenCategory == null ? (
+                <div>
                     <h1>Choose category</h1>
                     <button onClick={handleCategory}>Movie</button>
                     <button onClick={handleCategory}>Sport</button>
                     <button onClick={handleCategory}>Biology</button>
                 </div>
-            ):(
+            ) : (
                 <div>
                     <h1>Category: {chosenCategory}</h1>
-                    <Animation setValue = {setTimer}/>{/*Receives timer data from child component(Animation.js)*/}
+                    <Animation setValue={setTimer}/>{/*Receives timer data from child component(Animation.js)*/}
                     <div className="grid-container">
-                        {(() => {
-                            if (timer === 0) {
-                                return <div className="score-board">Time is up! Hangman is now dead. Your score: {score}</div>
-                            } else if(scoreBoard){
-                                return <div className="score-board">Hangman survived! Your score: {score}</div>
-                            } else {
-                                return <div className='quiz-container'>
-                                    <div className='question-text'>{questionAnswers[currentIndex]?.question}</div>
-                                    <div className='answer-section'>
-                                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.correctAnswer}</button>
-                                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.answer2}</button>
-                                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.answer3}</button>
-                                        <button onClick={handleAnswer}>{questionAnswers[currentIndex]?.answer4}</button>
-                                        <div>{text}</div>
-                                    </div>
-                                </div>
-                            }
-                        })()}
+                        {GameAreaSwitch()}
                     </div>
-
-
                 </div>
 
             )}
-
-
 
         </div>
     )
