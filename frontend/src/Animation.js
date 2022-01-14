98bd813e30e24cbdf7698df01bd79aeaa066f9c2
Naoya Irikura <naoiri2001@skola.goteborg.se>
2022-01-14 12:34:56 +0100

    Delete unused import

--- frontend/src/Animation.js
+++ frontend/src/Animation.js
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from "react";
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {FaRegSmile, FaRegMeh, FaRegFrownOpen, FaRegFlushed, FaRegDizzy, FaSkull} from 'react-icons/fa'
 
 function Animation(props) {
