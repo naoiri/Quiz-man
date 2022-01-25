60009b0c33876793ea90d04a95f18e11d1dafd45
Naoya Irikura <naoiri2001@skola.goteborg.se>
2022-01-25 11:48:28 +0100

    Better UI for choosing the correct answer

--- frontend/src/CreateQuestion.js
+++ frontend/src/CreateQuestion.js
@@ -4,10 +4,20 @@ function CreateQuestion() {
 
     const axios = require('axios');
     let cid;
+    let correctAnswer = '';
 
     async function postData(e) {
 
         cid = e.target.category.value;
+        if(e.target.correctanswer.value === '1'){
+            correctAnswer = e.target.answer1.value;
+        } else if(e.target.correctanswer.value === '2') {
+            correctAnswer = e.target.answer2.value;
+        } else if(e.target.correctanswer.value === '3') {
+            correctAnswer = e.target.answer3.value;
+        } else if(e.target.correctanswer.value === '4') {
+            correctAnswer = e.target.answer4.value;
+        }
 
         axios.post('http://localhost:8080/questionAnswers', {
             category: {
@@ -18,7 +28,7 @@ function CreateQuestion() {
             answer2: e.target.answer2.value,
             answer3: e.target.answer3.value,
             answer4: e.target.answer4.value,
-            correctAnswer: e.target.correctanswer.value
+            correctAnswer: correctAnswer
         }).then(function (response) {
             console.log(response);
         })
@@ -34,7 +44,7 @@ function CreateQuestion() {
                 <input type="text" name="answer2" placeholder="answer2"/>
                 <input type="text" name="answer3" placeholder="answer3"/>
                 <input type="text" name="answer4" placeholder="answer4"/>
-                <input type="text" name="correctanswer" placeholder="correct answer"/>
+                <input type="number" name="correctanswer" placeholder="correct answer" min="1" max="4"/>
                 <button>Submit</button>
             </form>
         </div>
